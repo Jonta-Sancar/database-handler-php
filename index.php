7277282371f5b0fbcdc2b41a9b2446fa454e5cde
Jonta Sancar <jontasancar@gmail.com>
2024-01-18 12:11:49 -0300

    alguns testes

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 
 include_once("./Handlers/Connection.php");
 include_once("./Handlers/SQL_CRUD.php");
-// use Handlers\Connection;
-// use Handlers\SQL_CRUD;
+use Handlers\Connection;
+use Handlers\SQL_CRUD;
 
-// $PDO = new Connection("localhost", "root", "1234", "teste");
+$PDO = new Connection("localhost", "root", "1234", "teste");
 
-// $crud = new SQL_CRUD("localhost", "root", "1234", "teste");
-// $response = $crud->execUpdate("pessoas", ["profissao"=> "pedreiro"],[['id', 17]]);
+$crud = new SQL_CRUD("localhost", "root", "1234", "teste");
+$response = $crud->execDelete("pessoas", [["id", 20]]);
 
-// var_dump($response);
+var_dump($response);
 // var_dump($PDO->executeSQL($response['SQL'], ...$response['VALUES']));
