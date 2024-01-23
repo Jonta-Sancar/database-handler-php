a651b21b75bc1966742400a2cce98f970a08f249
Jonta Sancar <jontasancar@gmail.com>
2024-01-23 15:03:07 -0300

    finalização de classe para manipulação de tabela específica

--- Handlers/CrudSpecificTable.php
+++ Handlers/CrudSpecificTable.php
@@ -4,11 +4,13 @@ namespace Handlers;
 use Handlers\SQL_CRUD;
 
 class CrudSpecificTable extends SQL_CRUD{
-  private Array $columns;
-  private Array $columns_for_registration;
-  private Array $columns_for_listing;
+  protected String $table;
+  protected Array $columns;
+  protected Array $columns_for_registration;
+  protected Array $columns_for_listing;
 
-  public function __construct(String $tabela, Array $columns, String $host = null, String $db_user = null, String $db_pass = null, String $db_name = null, String $db_drive = 'mysql') {
+  public function __construct(String $table, Array $columns, String $host = null, String $db_user = null, String $db_pass = null, String $db_name = null, String $db_drive = 'mysql') {
+    $this->table = $table;
     $this->db_host = $host;
     $this->db_user = $db_user;
     $this->db_pass = $db_pass;
@@ -26,5 +28,33 @@ class CrudSpecificTable extends SQL_CRUD{
         array_push($this->columns_for_listing, $column);
       }
     }
+  }
+
+  public function tableInsert(Array $data) : Object{
+    return $this->execInsert($this->table, $data);
+  }
+
+  public function tableSelect(String|Array|Null $columns = "*", Array|Null $conditions = null, Array|String|Null $group_by = null, Array|String|Null $order_by = null, String|Null $order_direction = "<", String|Int|Null $limit_min = 100, String|Int|Null $limit_max = null) : Object{
+    return $this->execSelect($this->table, $columns, $conditions, $group_by, $order_by, $order_direction, $limit_min, $limit_max);
+  }
+
+  public function tableUpdate(Array $data, Array|String|Null $conditions = null) : Object{
+    return $this->execUpdate($this->table, $data, $conditions);
+  }
+
+  public function tableDelete(Array|String|Null $conditions = null) : Object{
+    return $this->execDelete($this->table, $conditions);
+  }
+
+  public function returnsAllColumnsOfTable(){
+    return $this->columns;
+  }
+
+  public function returnsColumnsForRegistration(){
+    return $this->columns_for_registration;
+  }
+
+  public function returnsColumnsForListing(){
+    return $this->columns_for_listing;
   }
 }
