17bfc6a0f96173152fc6f677554f75c193a66f6e
Jonta Sancar <jontasancar@gmail.com>
2024-02-05 12:35:25 -0300

    optimize file

--- Handlers/CrudSpecificTable.php
+++ Handlers/CrudSpecificTable.php
@@ -5,23 +5,25 @@ use Handlers\SQL_CRUD;
 
 class CrudSpecificTable extends SQL_CRUD{
   protected String $table;
-  protected Array $columns;
-  protected Array $columns_for_registration;
-  protected Array $columns_for_listing;
+  protected Array $columns = [];
+  protected Array $columns_for_registration = [];
+  protected Array $columns_for_editing = [];
+  protected Array $columns_for_listing = [];
 
-  public function __construct(String $table, Array $columns, String $host = null, String $db_user = null, String $db_pass = null, String $db_name = null, String $db_drive = 'mysql') {
+  public function __construct(String $table, Array $columns, String $db_host = null, String $db_user = null, String $db_pass = null, String $db_name = null, String $db_drive = 'mysql') {
     $this->table = $table;
-    $this->db_host = $host;
-    $this->db_user = $db_user;
-    $this->db_pass = $db_pass;
-    $this->db_name = $db_name;
-    $this->db_drive = $db_drive;
+
+    parent::__construct($db_host, $db_user, $db_pass, $db_name, $db_drive);
 
     foreach ($columns as $column => $properties) {
       array_push($this->columns, $column);
 
       if(array_search("registration", $properties) !== false) {
         array_push($this->columns_for_registration, $column);
+      }
+      
+      if(array_search("editing", $properties) !== false){
+        array_push($this->columns_for_editing, $column);
       }
       
       if(array_search("listing", $properties) !== false){
@@ -52,6 +54,10 @@ class CrudSpecificTable extends SQL_CRUD{
 
   public function returnsColumnsForRegistration(){
     return $this->columns_for_registration;
+  }
+
+  public function returnsColumnsForEditing(){
+    return $this->columns_for_editing;
   }
 
   public function returnsColumnsForListing(){
